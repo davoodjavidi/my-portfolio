381fafc9970ac9b352c01a786b6176bf8231df32
davood javidi <javididavid1993@gmail.com>
2026-02-14 22:55:31 +0330

    edit

--- app/page.js
+++ app/page.js
@@ -12,8 +12,6 @@ export default function Home() {
       <Navbar />
       <Header />
       <About />
-      {/* <Services /> */}
-      {/* <Work /> */}
       <Contact />
       <Footer />
     </>
